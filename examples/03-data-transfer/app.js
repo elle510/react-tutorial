ac92db52a0414d94e4d8f56f647290f3bb0a604b
elle510 <elle0510@gmail.com>
2016-03-01 21:04:52 +0900

    add examples

--- examples/03-data-transfer/app.js
+++ examples/03-data-transfer/app.js
@@ -1,2 +1,14 @@
 // Component Data Transfer
 // Communicate Between Components
+var React = require('react');
+var ReactDom = require('react-dom');
+
+var Main = React.createClass({
+    render: function() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+});
+
+ReactDom.render(<Main />, document.getElementById('app'));
